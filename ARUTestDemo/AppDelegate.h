dedbc43e2f1c157fb931622cd81e9bfbf02affbe
orekiaru <orekiaru@foxmail.com>
2020-03-14 01:50:02 -0700

    Initial Commit

---
+++ ARUTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ARUTestDemo
+//
+//  Created by aru oreki on 2020/3/14.
+//  Copyright © 2020 aru oreki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ARUTestDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ARUTestDemo
+//
+//  Created by aru oreki on 2020/3/14.
+//  Copyright © 2020 aru oreki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
